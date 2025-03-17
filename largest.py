8c84698e3cac546d6aee566dfc3ce2c921f0e7e4
krish2236 <krish02062006@gmail.com>
2025-03-17 14:59:05 +0530

    Update largest.py

--- largest.py
+++ largest.py
@@ -1,9 +1,6 @@
 def find_largest(num1, num2, num3):
-   
     return max(num1, num2, num3)
-
 if __name__ == "__main__":
-    
     number1 = float(input("Enter first number: "))
     number2 = float(input("Enter second number: "))
     number3 = float(input("Enter third number: "))
