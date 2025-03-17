1fb003a54f3c983087449af78f3b75154df4fcae
krish2236 <krish02062006@gmail.com>
2025-03-17 15:14:24 +0530

    Create Data_Structure.py

---
+++ Data_Structure.py
@@ -0,0 +1,6 @@
+mydict = { "Name:" : "john" , "Age:" : 19 }
+mymarks = ( 90, 80, 95 , 93, 85 )
+myset = { 20 , 20 , 40 }
+print("Dictionary:", mydict)
+print("Marks Tuple:", mymarks)
+print("Set:", myset)
