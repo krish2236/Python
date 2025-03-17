5941a4bf1738d938ed1a790284efda8466ff8b42
krish2236 <krish02062006@gmail.com>
2025-03-17 14:58:28 +0530

    Update pallindrome.py

--- pallindrome.py
+++ pallindrome.py
@@ -1,10 +1,7 @@
 import string
-
 def is_palindrome(s):
-    # Remove spaces, punctuation, and convert to lowercase
     s = ''.join(e for e in s if e.isalnum()).lower()
     return s == s[::-1]
-
 input_string = input("Enter a string to check if it's a palindrome: ")
 if is_palindrome(input_string):
     print("The string is a palindrome.")
