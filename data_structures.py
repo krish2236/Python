92fd7d0700e19a987dff60bf3231e692fd0ee314
krish2236 <krish02062006@gmail.com>
2025-03-17 14:37:43 +0530

    Create data_structures.py

---
+++ data_structures.py
@@ -0,0 +1,9 @@
+my_list = [1, 2, 3, 4, 5]
+my_tuple = (1, 2, 3, 4, 5)
+my_set = {1, 2, 3, 4, 5}
+my_dict = {"one": 1, "two": 2, "three": 3}
+
+print("List:", my_list)
+print("Tuple:", my_tuple)
+print("Set:", my_set)
+print("Dictionary:", my_dict)
