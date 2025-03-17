9870b546eda65278963636d8074d05765ffe2be6
krish2236 <krish02062006@gmail.com>
2025-03-17 15:10:50 +0530

    Delete pallindrome.py

--- pallindrome.py
+++
@@ -1,9 +0,0 @@
-import string
-def is_palindrome(s):
-    s = ''.join(e for e in s if e.isalnum()).lower()
-    return s == s[::-1]
-input_string = input("Enter a string to check if it's a palindrome: ")
-if is_palindrome(input_string):
-    print("The string is a palindrome.")
-else:
-    print("The string is not a palindrome.")
