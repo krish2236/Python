951f011650dc23632da4e970a207a49cd7aeb2b2
Krish Pradeep <krish02062006@gmail.com>
2025-03-24 12:29:34 +0530

    Add files via upload

---
+++ Amount.py
@@ -0,0 +1,10 @@
+balance = 5000
+withdraw = int(input("Enter amount to withdraw: "))
+
+if withdraw < 0:
+    print("Invalid amount! Enter a positive number.")
+elif withdraw > balance:
+    print("Insufficient balance!")
+else:
+    balance -= withdraw
+    print("Withdrawal successful. Remaining balance:", balance)
